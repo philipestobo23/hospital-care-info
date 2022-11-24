576bbb73f8d78c357c32f54028f4e54cce67da34
Christian Dave M. Bate <cbate@ssct.edu.ph>
2022-11-23 20:46:31 -0400

    fulltemplate

--- resources/views/doctors/create.blade.php
+++ resources/views/doctors/create.blade.php
@@ -4,6 +4,79 @@
 
 @section('content')
     <div class="container-fluid px-4">
-        Add Specialist
+        <div class="row">
+            <p></p>
+            <div class="text-center">
+                <h3 class="text-primary">REGISTER SPECIALIST</h3>
+            </div>
+            <div class="col-3">
+                <p></p>
+                <div class="p-2">
+                    <img src="https://source.unsplash.com/collection/190727/1000x900" class="d-block img-fluid" alt="image">
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control" style="text-align: center;">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <p></p>
+                <div class="p-2">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text bg-primary"><i class="bi bi-person-plus-fill text-white"></i></span>
+                        <input type="text" class="form-control" placeholder="Name Extension">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="First name">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Middle Name">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Last Name">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Address">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Contact Number">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6">
+                <p></p>
+                <div class="p-2">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Gender">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Age">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Nick Name">
+                    </div>
+                    <div class="input-group mb-3">
+                        <label style="color: blue;" class="form-control" for="meeting-time">Schedule Availability Date &
+                            Time:</label>
+                        &nbsp&nbsp
+                        <input class="form-control" type="datetime-local" id="meeting-time" name="meeting-time"
+                            value="2018-06-12T19:30" min="2018-06-07T00:00" max="2018-06-14T00:00">
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text bg-primary">
+                            <i class="bi bi-unlock-fill text-white"></i>
+                        </span>
+                        <input type="text" class="form-control" placeholder="Specialist Field">
+                    </div>
+                </div>
+            </div>
+            <div class="d-grid col-3 mx-auto">
+                <button class="btn btn-primary" type="button"><span></span> Register to database</button>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -3,7 +3,101 @@
 @section('title', 'View Patients')
 
 @section('content')
-    <div class="container-fluid px-4">
-        View Patients
+<div class="container-fluid px-4">
+    <div class="row">
+        <p></p>
+        <div class="text-center">
+            <h3 class="text-primary">VIEW PATIENT</h3>
+        </div>
+        <div class="col-3">
+            <p></p>
+            <div class="p-2">
+                <img src="https://source.unsplash.com/collection/190727/1000x900" class="d-block img-fluid" alt="image">
+                <div class="input-group mb-3">
+
+                </div>
+                <div class="d-grid col-12 mx-auto">
+                    <button class="btn btn-primary" type="button"><span></span> Download QRCODE</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-9">
+            <p></p>
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names...">
+
+            <table id="myTable">
+                <tr class="header">
+                    <th style="width:auto">Patient Name</th>
+                    <th style="width:auto">Doctor Assigned</th>
+                    <th style="width:auto">Time Schedule</th>
+                    <th style="width:auto">Action</th>
+                </tr>
+                <tr>
+                    <td>Christian Nasakit</td>
+                    <td>Dr. Bate, Christian</td>
+                    <td>7-8 am</td>
+                    <td>Select</td>
+                </tr>
+            </table>
+
+            <script>
+                function myFunction() {
+                    var input, filter, table, tr, td, i, txtValue;
+                    input = document.getElementById("myInput");
+                    filter = input.value.toUpperCase();
+                    table = document.getElementById("myTable");
+                    tr = table.getElementsByTagName("tr");
+                    for (i = 0; i < tr.length; i++) {
+                        td = tr[i].getElementsByTagName("td")[0];
+                        if (td) {
+                            txtValue = td.textContent || td.innerText;
+                            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                                tr[i].style.display = "";
+                            } else {
+                                tr[i].style.display = "none";
+                            }
+                        }
+                    }
+                }
+            </script>
+            <style>
+                * {
+                  box-sizing: border-box;
+                }
+                
+                #myInput {
+                  background-image: url('/css/searchicon.png');
+                  background-position: 10px 10px;
+                  background-repeat: no-repeat;
+                  width: 100%;
+                  font-size: 16px;
+                  padding: 12px 20px 12px 40px;
+                  border: 1px solid #ddd;
+                  margin-bottom: 12px;
+                }
+                
+                #myTable {
+                  border-collapse: collapse;
+                  width: 100%;
+                  border: 1px solid #ddd;
+                  font-size: 18px;
+                }
+                
+                #myTable th, #myTable td {
+                  text-align: left;
+                  padding: 12px;
+                }
+                
+                #myTable tr {
+                  border-bottom: 1px solid #ddd;
+                }
+                
+                #myTable tr.header, #myTable tr:hover {
+                  background-color: #f1f1f1;
+                }
+                </style>
+        </div>
     </div>
+</div>
 @endsection
